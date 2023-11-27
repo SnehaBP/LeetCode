5c71c4bc86a0ba7f793d2d068eb7caadc4030e5b
SnehaBP <138276415+SnehaBP@users.noreply.github.com>
2023-11-27 20:06:07 +0530

    Time: 10 ms (92.55%), Space: 13.4 MB (27.04%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        res = 0
+        for c in columnTitle:
+            res = res * 26 + ord(c) - ord('@')
+        return res
+        
