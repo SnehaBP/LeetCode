18a2b529bc2d0e04028a45d6bf302c5074429d59
SnehaBP <138276415+SnehaBP@users.noreply.github.com>
2023-12-06 22:00:22 +0530

    Time: 38 ms (65.98%), Space: 16.3 MB (7.77%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,10 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        def trapezoid(a: int, b: int) -> int:
+            return (a + b) * (b - a + 1) // 2
+
+        weeks = n // 7
+        firstWeek = trapezoid(1, 7)
+        lastFullWeek = trapezoid(1 + weeks - 1, 7 + weeks - 1)
+        remainingDays = trapezoid(1 + weeks, n % 7 + weeks)
+        return (firstWeek + lastFullWeek) * weeks // 2 + remainingDays
