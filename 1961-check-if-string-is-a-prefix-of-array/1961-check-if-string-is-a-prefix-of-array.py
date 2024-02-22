e048ed84e8272d15e7261a2c682b562ef528607a
Sneha B P <138276415+SnehaBP@users.noreply.github.com>
2024-02-22 10:08:46 +0530

    Time: 39 ms (63.45%), Space: 16.6 MB (86.47%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.py
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.py
@@ -1,10 +1,8 @@
 class Solution:
     def isPrefixString(self, s: str, words: List[str]) -> bool:
-        flag=0
         concatinatedWords=''
         for word in words:
             concatinatedWords=concatinatedWords+word
             if concatinatedWords==s:
-                flag=1
                 break
-        return flag==1
+        return concatinatedWords==s
