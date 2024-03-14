c04cce3d637a3a45a7ecf1aa68d074114a82aa9f
Sneha B P <138276415+SnehaBP@users.noreply.github.com>
2024-03-14 21:06:32 +0530

    Time: 27 ms (99.54%), Space: 16.5 MB (94.19%) - LeetHub

--- 3038-maximum-number-of-operations-with-the-same-score-i/3038-maximum-number-of-operations-with-the-same-score-i.py
+++ 3038-maximum-number-of-operations-with-the-same-score-i/3038-maximum-number-of-operations-with-the-same-score-i.py
@@ -4,7 +4,6 @@ class Solution:
         sumOfTowNums=nums[0]+nums[1]
         for i in range(0,len(nums)-1,2):
             if (nums[i]+nums[i+1])==sumOfTowNums:
-                print(nums[i]+nums[i+1])
                 count+=1
             else:
                 break
