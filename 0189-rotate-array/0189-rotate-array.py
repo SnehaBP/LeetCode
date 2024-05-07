f9ab0b87e30c96483da681353ed73a09ff424ddb
Sneha B P <138276415+SnehaBP@users.noreply.github.com>
2024-05-07 13:21:15 +0530

    Time: 151 ms (86.32%), Space: 28 MB (68.85%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,6 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
-        steps=k%n
-        nums[:]=nums[-steps:]+nums[:-steps]
-        return nums
+        k=k%n
+        nums[:]=nums[-k:]+nums[:-k]
