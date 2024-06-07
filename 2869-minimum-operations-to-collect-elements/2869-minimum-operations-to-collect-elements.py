bb7759e5dced32a7e52e7bba381e05bdbb06f519
Sneha B P <138276415+SnehaBP@users.noreply.github.com>
2024-06-07 22:57:39 +0530

    Time: 48 ms (16.67%), Space: 16.5 MB (90.36%) - LeetHub

---
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        numsRemoved = []
+        for i in range(len(nums)-1 , -1, -1):
+            numsRemoved.append(nums[i])
+            print(numsRemoved)
+            flag = 0
+            for j in range(1 , k+1):
+                if j not in numsRemoved:
+                    flag = 0
+                    break
+                else:
+                    flag = 1
+            if flag == 1:
+                return len(numsRemoved)
