32b2fb5ee2413e245c5d9d1c8bae72d2b0c4888c
Sneha B P <138276415+SnehaBP@users.noreply.github.com>
2024-06-12 19:38:35 +0530

    Time: 38 ms (53.24%), Space: 16.7 MB (25.55%) - LeetHub

---
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfChild(self, n: int, k: int) -> int:
+        count = 0
+        j = -1
+        while count <= k+1:
+            for i in range(j+1, n):
+                print(i)
+                count +=1
+                if count == k+1:
+                    return i 
+            for j in range(n-2, -1, -1):
+                print(j)
+                count +=1
+                if count == k+1:
+                    return j
